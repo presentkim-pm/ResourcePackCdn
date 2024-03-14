bfcf1bb6d953b65bb8b6810078fb3966a0059e4c
PresentKim <debe3721@gmail.com>
2024-03-14 15:36:46 +0900

    :sparkles: Implement main features

--- src/kim/present/resourcepackcdn/Main.php
+++ src/kim/present/resourcepackcdn/Main.php
@@ -27,9 +27,73 @@ declare(strict_types=1);
 
 namespace kim\present\resourcepackcdn;
 
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketSendEvent;
+use pocketmine\network\mcpe\protocol\ResourcePacksInfoPacket;
 use pocketmine\plugin\PluginBase;
+use pocketmine\resourcepacks\ZippedResourcePack;
+use pocketmine\scheduler\ClosureTask;
+use Symfony\Component\Filesystem\Path;
 
-final class Main extends PluginBase{
+use function array_merge;
+use function rtrim;
+use function strlen;
+use function strrchr;
+use function substr;
 
-	protected function onEnable() : void{}
+final class Main extends PluginBase implements Listener{
+	/** @var string[] */
+	private array $cdnUrls = [];
+
+	private string $cdnBaseUrl;
+	private string $cdnBaseDir;
+	private bool $removeExtension;
+
+	protected function onEnable() : void{
+		$config = $this->getConfig();
+		$serverDir = $this->getServer()->getDataPath();
+		$this->cdnBaseUrl = rtrim($config->get("base_url", "https://cdn.example.com/"), "/") . "/";
+		$this->cdnBaseDir = rtrim(Path::join($serverDir, $config->get("base_dir", "./resource_packs")), "/") . "/";
+		$this->removeExtension = (bool) $config->get("remove_extension", false);
+
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+		// Run matching immediately to check for resource packs added before this plugin.
+		$this->matchPackInCdnDir();
+
+		// Run matching after 1 tick to check for resource packs added later than this plugin.
+		$this->getScheduler()->scheduleDelayedTask(new ClosureTask(fn() => $this->matchPackInCdnDir()), 1);
+	}
+
+	private function matchPackInCdnDir() : void{
+		$resourcePackManager = $this->getServer()->getResourcePackManager();
+		foreach($resourcePackManager->getResourceStack() as $pack){
+			if(
+				$pack instanceof ZippedResourcePack
+				&& Path::isBasePath($this->cdnBaseDir, $pack->getPath())
+			){
+				$uuid = $pack->getPackId();
+				$version = $pack->getPackVersion();
+				$key = $uuid . "_" . $version;
+				if(isset($this->cdnUrls[$key])){
+					continue;
+				}
+				$cdnUrl = $this->cdnBaseUrl . Path::makeRelative($pack->getPath(), $this->cdnBaseDir);
+				if($this->removeExtension){
+					$cdnUrl = substr($cdnUrl, 0, -strlen(strrchr($cdnUrl, ".")));
+				}
+				$this->getLogger()->info("Registered CDN URL for {$pack->getPackName()}_v$version ($uuid) : $cdnUrl");
+				$this->cdnUrls[$key] = $cdnUrl;
+			}
+		}
+	}
+
+	/** @priority HIGHEST */
+	public function onDataPacketSend(DataPacketSendEvent $event) : void{
+		foreach($event->getPackets() as $packet){
+			if($packet instanceof ResourcePacksInfoPacket){
+				$packet->cdnUrls = array_merge($this->cdnUrls, $packet->cdnUrls);
+			}
+		}
+	}
 }
